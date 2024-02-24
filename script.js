83c71e9e97d6e1a42cb03361e86398d8280c74d5
sekencois <161124134+sekencois@users.noreply.github.com>
2024-02-24 23:37:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const audio = document.getElementById('audio');
 const record = document.querySelector('.record');
 
+audio.volume = 0.5; // Mengatur volume audio
+
 audio.addEventListener('play', function() {
   record.style.animationPlayState = 'running';
 });
 
 audio.addEventListener('pause', function() {
   record.style.animationPlayState = 'paused';
+});
+
+// Otomatis memutar audio ketika dokumen dimuat
+document.addEventListener('DOMContentLoaded', function() {
+  audio.play();
 });
